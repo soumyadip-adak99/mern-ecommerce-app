3d8a644f5a43f4d9c27a32ed61646c94d9e079ff
unknown <soumya.dip.adak@outlook.com>
2025-12-20 15:33:12 +0530

    update

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/appFeatures/authSlice";
-import productReducer from "../features/appFeatures/productSlice";
-import orderReducer from "../features/appFeatures/orderSlice";
-import cartReducer from "../features/appFeatures/cartSlice";
+
+import authReducer from "../features/appFeatures/authSlice.js";
+import productReducer from "../features/appFeatures/productSlice.js";
+import orderReducer from "../features/appFeatures/orderSlice.js";
+import cartReducer from "../features/appFeatures/cartSlice.js";
 
 export const store = configureStore({
     reducer: {
